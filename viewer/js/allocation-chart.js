66995da749c2bbe271f36856067a34ab03c7ddec
Andre-John Mas <andrejohn.mas@gmail.com>
2015-12-25 00:01:01 -0500

    Tweaks to allocaiton chart

--- viewer/js/allocation-chart.js
+++ viewer/js/allocation-chart.js
@@ -12,7 +12,7 @@ var SpectrumChart = (function(containerElementId, bands) {
 	};
 
 	function drawFullChart() {
-		var height, spacing, i;
+		var height, spacing, i, text;
 
 		var ranges = [
 			[3 * Math.pow(10, 3), 300 * Math.pow(10, 3)],
@@ -27,7 +27,7 @@ var SpectrumChart = (function(containerElementId, bands) {
 		];
 
 		spacing = 30;
-		height = (boundingBox.height - (spacing * (ranges.length - 1))) / ranges.length;
+		height = (boundingBox.height) / ranges.length;
 
 		for (i = 0; i < ranges.length; i++) {
 
@@ -37,9 +37,11 @@ var SpectrumChart = (function(containerElementId, bands) {
 				width: boundingBox.width,
 				height: height
 			};
-			drawChart(ranges[i], box);
+
+			drawChart(ranges[i], box, true);
 
 		}
+
 	};
 
 	function getServiceAndColor(text) {
@@ -56,28 +58,58 @@ var SpectrumChart = (function(containerElementId, bands) {
 		return (candidate[0] !== '' ? candidate : undefined);
 	};
 
-	function drawChart(range, boundingBox) {
-		var i, j, width, height, offsetX = 0, offsetY, len, service, services;
-		var logarithmic = false;
+	function drawChart(range, boundingBox, logarithmic) {
+		var i, j, width, height, offsetX = 0, offsetY, len, service, services, text;
+		var lf, uf, drawOutline = true;
 
-		snap.rect(
-		boundingBox.x,
-		boundingBox.y,
-		boundingBox.width,
-		boundingBox.height).attr({
-			stroke: 'black',
-			fill: 'white'
-		});
 
+        // draw the chart outline
+
+        if (drawOutline) {
+            snap.rect(
+                boundingBox.x,
+                boundingBox.y,
+                boundingBox.width,
+                boundingBox.height -30).attr({
+                stroke: 'white',
+                fill: 'white',
+                class: 'outline'
+            });
+        }
+
+        // draw the range labels under the chart
+
+        height = boundingBox.height - 30;
+        width = boundingBox.width;
+
+        text = snap.text(boundingBox.x, boundingBox.y + height + 15, valueToMagnitude(range[0], 'Hz'));
+        text = snap.text(boundingBox.x + width, boundingBox.y + height + 15, valueToMagnitude(range[1], 'Hz'));
+        text.attr({ x: boundingBox.x + width - text.getBBox().width });
+
+        // calculate the range, depending on whether we are in linear or logarithmic mode
 
 		if (logarithmic) {
 			len = Math.log10(range[1]) - Math.log10(range[0]);
 		} else {
 			len = range[1] - range[0];
 		}
+        len = len * 1.0;
+
+
+
+        // draw the services. currently only support one table at a time.
+//        var cumWidth = 0, mLf, mUf =0;
 
 		for (i = 0; i < bands.length; i++) {
-			if (bands[i].uf > range[0] && bands[i].lf < range[1]) {
+
+// 		    if (bands[i].lf >= range[1]) {
+// 		        break;
+// 		    }
+
+
+			if (bands[i].uf >= range[0] && bands[i].lf <= range[1]) {
+
+//                console.log(bands[i].lf, bands[i].uf, JSON.stringify(bands[i].services));
 
 				if (logarithmic) {
 					lf = Math.log10(bands[i].lf);
@@ -86,25 +118,56 @@ var SpectrumChart = (function(containerElementId, bands) {
 					if (lf === Infinity || lf === -Infinity) {
 						lf = 0;
 					}
-					width = (boundingBox.width / len) * (uf - lf);
+
+	                if (lf < Math.log10(range[0])) {
+				        lf = Math.log10(range[0]);
+				    }
+				    if (uf > Math.log10(range[1]))   {
+				        uf = Math.log10(range[1]);
+				    }
 				} else {
-					width = (boundingBox.width / len) * (bands[i].uf - bands[i].lf);
+				    lf = parseInt(bands[i].lf);
+				    uf = parseInt(bands[i].uf);
+
+				    if (lf < range[0]) {
+				        lf = range[0];
+				    }
+				    if (uf > range[1]) {
+				        uf = range[1];
+				    }
 				}
 
-				if (bands[i].services && bands[i].services.length > 0) {
-					services = bands[i].services;
-					offsetY = boundingBox.y;
+				width = boundingBox.width * ((uf - lf)/len);
 
+				if (bands[i].services && bands[i].services.length > 0) {
+
+					services = bands[i].services;
+
+                    // Sort the entries, so the primary services are displayed first
+                    services.sort(function (x, y) {
+
+                        if (y.cat === 'p' && x.cat !== 'p') {
+                            return 1;
+                        }
+
+                        if (x.cat === 'p' && y.cat !== 'p') {
+                            return -1;
+                        }
+
+                        return x.desc.localeCompare(y.desc);
+                    });
+
+					offsetY = boundingBox.y;
 					for (j = 0; j < services.length; j++) {
-						height = boundingBox.height / services.length;
-						drawServce(bands[i].services[j].desc, offsetX, offsetY, width, height);
+						height = (boundingBox.height-30) / services.length;
+						drawServce(bands[i].services[j], offsetX, offsetY, width, height);
 						offsetY = offsetY + height;
 					}
 
 				} else {
-					snap.rect(offsetX, boundingBox.y, width, boundingBox.height).attr({
+					snap.rect(offsetX, boundingBox.y, width, height).attr({
 						stroke: 'black',
-						fill: 'blue'
+						fill: 'white'
 					});
 				}
 
@@ -112,6 +175,7 @@ var SpectrumChart = (function(containerElementId, bands) {
 
 			}
 		}
+
 	}
 
 	function drawServce(service, x, y, width, height) {
@@ -119,14 +183,16 @@ var SpectrumChart = (function(containerElementId, bands) {
 		var fillColor = 'white';
 		var serviceName = '';
 
-		serviceAndColor = getServiceAndColor(service);
+		serviceAndColor = getServiceAndColor(service.desc);
 
 		if (serviceAndColor) {
 			fillColor = serviceAndColor[1];
 			serviceName = serviceAndColor[0];
-			if (fillColor === undefined) {
-			     console.log(serviceAndColor, fillColor, serviceName );
-			}
+		} else {
+		    if (service !== '-') {
+		        serviceName = service.desc;
+		    }
+
 		}
 
 		rect = snap.rect(x, y, width, height).attr({
@@ -136,30 +202,37 @@ var SpectrumChart = (function(containerElementId, bands) {
 			'class': 'service ' + serviceName.replace(' ','')
 		});
 
-        text = snap.text(-200, -200, ' ' + serviceName + ' ');
+        text = snap.text(-200, -200, '');
+
+        if (service.cat === 'p') {
+            serviceName = serviceName.toUpperCase();
+        }
+
+        text.append( Snap.parse('<tspan> ' + serviceName + ' </tspan>') );
+
         text.attr({
-            x: rect.getBBox().x + (width / 2 - (text.getBBox().width / 2)),
-            y: rect.getBBox().y + (height / 2 + (text.getBBox().height / 2)),
-            'class': 'service'
+            x: x + (width / 2 - (text.getBBox().width / 2)),
+            y: y + (height / 2 + (text.getBBox().height / 2)),
+            width: width,
+            'class': 'service ' //+ serviceName.replace(' ','')
         });
 
-        if (text.getBBox().width > rect.getBBox().width) {
+        var textBBox = text.getBBox();
+        if (textBBox.width > width) {
             var transform = '';
-            if (rect.getBBox().width > rect.getBBox().height) {
-                transform = 's' + (rect.getBBox().width / text.getBBox().width);
+            if (width > height) {
+                transform = 's' + (width / textBBox.width);
             } else {
                 transform += 'r-90';
-
-                if (text.getBBox().width > rect.getBBox().height) {
-                    transform += ' s' + (text.getBBox().height / rect.getBBox().height);
+                if (textBBox.width > height) {
+                    transform += ' s' + ((height / textBBox.width) * 0.9);
                 } else {
-                    transform += ' s' + (rect.getBBox().width / text.getBBox().width);
+                    transform += ' s' + ((width / textBBox.width) * 0.9);
                 }
             }
             text.transform(transform);
 
         }
-
 
 	}
 
@@ -181,7 +254,7 @@ var SpectrumChart = (function(containerElementId, bands) {
 	drawFullChart();
 });
 
-
+var allocationData;
 $(document).ready(function() {
     var i;
 
@@ -195,13 +268,19 @@ $(document).ready(function() {
             );
 	}
 
+    var clickActive = false;
+    var activeService = undefined;
 	$('li.key').on('mouseenter', function () {
 
-	     Snap.selectAll('.service').forEach( function (element, b) {
-	     	 element.addClass('rectHidden');
+         if (clickActive) {
+             return;
+         }
+
+	     Snap.selectAll('.outline').forEach( function (element, b) {
+	     	 element.addClass('outlineActive');
 	     });
 
-	     Snap.selectAll('text').forEach( function (element, b) {
+	     Snap.selectAll('.service').forEach( function (element, b) {
 	     	 element.addClass('rectHidden');
 	     });
 
@@ -217,6 +296,14 @@ $(document).ready(function() {
 
 	$('li.key').on('mouseleave', function () {
 
+         if (clickActive) {
+             return;
+         }
+
+	     Snap.selectAll('.outline').forEach( function (element, b) {
+	     	 element.removeClass('outlineActive');
+	     });
+
 	     Snap.selectAll('.service').forEach( function (element, b) {
 	     	 element.removeClass('rectHidden');
 	     });
@@ -225,17 +312,93 @@ $(document).ready(function() {
 	     	 element.removeClass('rectHighlight');
 	     });
 
-	     Snap.selectAll('text').forEach( function (element, b) {
-	     	 element.removeClass('rectHidden');
-	     });
 	});
+
+	$('li.key').on('click', function () {
+
+         if (clickActive) {
+
+            if ( $(this).attr('id') === activeService ) {
+                 $(this).removeClass('legendItemActive');
+
+                 Snap.selectAll('.outline').forEach( function (element, b) {
+                     element.removeClass('outlineActive');
+                 });
+
+                 Snap.selectAll('.service').forEach( function (element, b) {
+                     element.removeClass('rectHidden');
+                 });
+
+                 Snap.selectAll('.service').forEach( function (element, b) {
+                     element.removeClass('rectHighlight');
+                 });
+
+                 activeService = undefined;
+                 clickActive = false;
+             }
+         } else {
+
+             $(this).addClass('legendItemActive');
+
+             Snap.selectAll('.outline').forEach( function (element, b) {
+                 element.addClass('outlineActive');
+             });
+
+             Snap.selectAll('.service').forEach( function (element, b) {
+                 element.addClass('rectHidden');
+             });
+
+//              Snap.selectAll('text').forEach( function (element, b) {
+//                  element.addClass('rectHidden');
+//              });
+
+             Snap.selectAll('.' + $(this).attr('id')).forEach( function (element, b) {
+                 element.removeClass('rectHidden');
+             });
+
+             Snap.selectAll('.' + $(this).attr('id')).forEach( function (element, b) {
+                 element.addClass('rectHighlight');
+             });
+
+             activeService = $(this).attr('id');
+             clickActive = true;
+
+         }
+
+
+	});
+
+	$('select[name=\'activity\']').on('change', function() {
+	    var tableIdx = $(this).val();
+	    $('#allocationChart').html('loading...');
+	    setTimeout(function() {
+    	    SpectrumChart('allocationChart', allocationData.tables[tableIdx].bands);
+    	    }, 0);
+	});
+
 
     loadAvailableRegions('#regionSelector', function(region) {
         var url = baseRestPath + '/tables/' + region + '/index.json';
-        $.getJSON(url, function(data) {
 
+        $.getJSON(url, function(data) {
+            var i, activitySelect;
+
+            allocationData = data;
             $('.region').html(data.metadata.name_en);
-         	$('#allocationChart').html('loading...');
+
+            activitySelect = $('select[name=\'activity\']');
+            if (data.tables.length === 1) {
+                activitySelect.html('<option>All</option>');
+                activitySelect.attr('disabled',true);
+            } else {
+                activitySelect.html('');
+                for (i=0; i<data.tables.length; i++) {
+                    activitySelect.append('<option value="' + i + '">' + data.tables[i].name + '</option>');
+                }
+                activitySelect.attr('disabled',false);
+            }
+
+            $('#allocationChart').html('loading...');
 
             setTimeout(function() {
                 var spectrumChart = SpectrumChart('allocationChart', data.tables[0].bands);
