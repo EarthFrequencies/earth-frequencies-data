35745ab6fa8e4b7cdc5ba5f5f92c4b5095664fd2
Julien Lhermitte <jrmlhermitte@gmail.com>
2021-12-17 18:35:21 -0500

    add js files

--- scripts/generate_protos.sh
+++ scripts/generate_protos.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-protoc -I=protos --python_out=python/frequencies_converter/protos protos/frequencies.proto
+protoc -I=protos --js_out=import_style=commonjs,binary:js/protos --python_out=python/frequencies_converter/protos protos/frequencies.proto
