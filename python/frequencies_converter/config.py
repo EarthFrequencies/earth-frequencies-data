078ec341690161d857d90cfa7bc8ddb0b16d63ad
Andre-John Mas <andrejohn.mas@gmail.com>
2022-03-04 14:30:00 -0500

    change file endings to tsv

--- python/frequencies_converter/config.py
+++ python/frequencies_converter/config.py
@@ -6,6 +6,6 @@ DATA_DIR = Path(__file__).parent.parent.parent / Path("data/allocation-tables")
 # TODO (jrmlhermitte): These should be renamed to .tsv
 #  (tab separated values).
 # Names for the csv files
-ALLOCATION_FILENAME = "allocations.txt"
-METADATA_FILENAME = "metadata.txt"
+ALLOCATION_FILENAME = "allocations.tsv"
+METADATA_FILENAME = "metadata.tsv"
 JSON_INDEX_FILENAME = "index.json"
